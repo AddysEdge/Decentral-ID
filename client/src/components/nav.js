0c771860333ae1f99f273c8e769efd8704a415cb
Addy <adityarana2523@gmail.com>
2026-08-13 23:49:13 -0700

    fix: detect admin section with startsWith so /admin/:index also hides user nav links

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -3,13 +3,15 @@ import { Link, useLocation } from "react-router-dom"
 import { TransactionContext } from "../context/TransactionContext"
 
 export default function Nav() {
-	const {currentAccount,isAdmin} = useContext(TransactionContext)
-	const {pathname} = useLocation()
+	const { currentAccount } = useContext(TransactionContext)
+	const { pathname } = useLocation()
+
+	const isAdminSection = pathname.startsWith('/admin')
 
 	return (
 			<div className='flex justify-between w-full p-10'>
 				<h1 className='text-3xl font-bold'>DecentralID</h1>
-				{pathname != '/admin' && (
+				{!isAdminSection && (
 				<div className='flex space-x-5'>
 					<Link
 						to='/home/addDocument'
