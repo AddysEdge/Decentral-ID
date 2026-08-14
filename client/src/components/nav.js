fe2ece5c34c02f20fc4c04cb947725952c8e5228
Addy <adityarana2523@gmail.com>
2026-08-13 23:50:47 -0700

    feat: highlight active nav link

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -8,21 +8,22 @@ export default function Nav() {
 
 	const isAdminSection = pathname.startsWith('/admin')
 
+	const linkClass = (path) =>
+		`p-3 rounded-md transition-colors ${
+			pathname === path
+				? 'bg-blue-500 text-white'
+				: 'bg-blue-300 hover:bg-blue-500'
+		}`
+
 	return (
 			<div className='flex justify-between w-full p-10'>
 				<h1 className='text-3xl font-bold'>DecentralID</h1>
 				{!isAdminSection && (
 				<div className='flex space-x-5'>
-					<Link
-						to='/home/addDocument'
-						className='p-3 bg-blue-300 rounded-md hover:bg-blue-500'
-					>
+					<Link to='/home/addDocument' className={linkClass('/home/addDocument')}>
 						Add Verification Request
 					</Link>
-					<Link
-						to='/home'
-						className='p-3 bg-blue-300 rounded-md hover:bg-blue-500'
-					>
+					<Link to='/home' className={linkClass('/home')}>
 						My Requests Status
 					</Link>
 				</div>)
