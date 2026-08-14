c24ffcfdfe58f0bf082eca8eb0ff60f965692ef2
Addy <adityarana2523@gmail.com>
2026-08-13 23:50:48 -0700

    fix: use className instead of class attribute in verifier.js

--- client/src/pages/verifier.js
+++ client/src/pages/verifier.js
@@ -28,46 +28,46 @@ const Verifier = () => {
 				<div className='flex flex-col items-center w-full h-full'>
 
 				<h1 className="pb-10 text-4xl">Verification Panel</h1>
-				<table class='w-2/4 rounded-lg border-2 border-gray'>
-					<thead class='bg-cyan-500'>
+				<table className='w-2/4 rounded-lg border-2 border-gray'>
+					<thead className='bg-cyan-500'>
 						<tr>
 							<th
 								scope='col'
-								class='text-sm font-medium text-white px-6 py-4 text-left'
+								className='text-sm font-medium text-white px-6 py-4 text-left'
 							>
 								Id
 							</th>
 							<th
 								scope='col'
-								class='text-sm font-medium text-white px-6 py-4 text-left'
+								className='text-sm font-medium text-white px-6 py-4 text-left'
 							>
 								Address
 							</th>
 							<th
 								scope='col'
-								class='text-sm font-medium text-white px-6 py-4 text-left'
+								className='text-sm font-medium text-white px-6 py-4 text-left'
 							>
 								Verified
 							</th>
-							<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
+							<td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
 							</td>
 						</tr>
 					</thead>
 					<tbody>
 						{verifierVReqList.map(({user, status}, index) => (
-							<tr class='bg-gray-100 border-b' key={index}>
-								<td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
+							<tr className='bg-gray-100 border-b' key={index}>
+								<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
 									{index+1}
 								</td>
-								<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
+								<td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
 									{user}
 								</td>
-								<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
+								<td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
 									{status === 0 && "PROCESSING"}
 									{status === 1 && "ACCEPTED"}
 									{status === -1 && "REJECTED"}
 								</td>
-								<td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
+								<td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
 								{status === 0 && <a
 									onClick = {() => {
 									navigate(`/admin/${index+1}`)
